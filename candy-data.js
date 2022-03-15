79b1bc80b863afd8c95a741dc3bc58757f00129f
Beau Elliott <belliott1516@gmail.com>
2022-03-15 11:07:42 -0700

    added candy array to candy data

---
+++ candy-data.js
@@ -0,0 +1,32 @@
+export const candyEl = [
+    {
+        name: 'Gummy Bears', 
+        sweetness: '4',
+        cost: '3',
+        description: 'chewy mammals in varying colors'
+    },
+    {
+        name: 'Skittles', 
+        sweetness: '5',
+        cost: '2',
+        description: 'Hard sugar shell with chewy center in varying colors'
+    },
+    {
+        name: 'Snickers', 
+        sweetness: '2',
+        cost: '3',
+        description: 'chocolate covered bar filled with nuget and peanuts'
+    },
+    {
+        name: 'M & M', 
+        sweetness: '2',
+        cost: '1',
+        description: 'Hard sugar outer shell with milk chocolate center'
+    },
+    {
+        name: 'Fun Dip', 
+        sweetness: '5',
+        cost: '2',
+        description: 'colored sugar with chalky sugar stick'
+    },
+];
