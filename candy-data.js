ca5895367ea622454b8201fe56efcb7f3eb0c1e3
Beau Elliott <belliott1516@gmail.com>
2022-03-15 11:10:12 -0700

    added color to candy data

--- candy-data.js
+++ candy-data.js
@@ -3,30 +3,35 @@ export const candyEl = [
         name: 'Gummy Bears', 
         sweetness: '4',
         cost: '3',
-        description: 'chewy mammals in varying colors'
+        description: 'chewy mammals in varying colors',
+        color: 'green'
     },
     {
         name: 'Skittles', 
         sweetness: '5',
         cost: '2',
-        description: 'Hard sugar shell with chewy center in varying colors'
+        description: 'Hard sugar shell with chewy center in varying colors',
+        color: 'red'
     },
     {
         name: 'Snickers', 
         sweetness: '2',
         cost: '3',
-        description: 'chocolate covered bar filled with nuget and peanuts'
+        description: 'chocolate covered bar filled with nuget and peanuts', 
+        color: 'brown'
     },
     {
         name: 'M & M', 
         sweetness: '2',
         cost: '1',
-        description: 'Hard sugar outer shell with milk chocolate center'
+        description: 'Hard sugar outer shell with milk chocolate center',
+        color: 'yellow'
     },
     {
         name: 'Fun Dip', 
         sweetness: '5',
         cost: '2',
-        description: 'colored sugar with chalky sugar stick'
+        description: 'colored sugar with chalky sugar stick',
+        color: 'purple'
     },
 ];
